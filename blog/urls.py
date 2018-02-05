42f0d259b87e0eec480c90ca4842408caa9d2a4b
Timon Missoorten <timonmissoorten@Timons-Mac-mini.local>
2018-02-05 15:22:35 +0100

    Updated blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
